9b0c10643eff7d638d3f113042bc72bb59cdabfa
Bruno Silva <brunosilva@nunos-mac-mini.sinfic.pt>
2017-04-05 15:49:04 +0100

    testing

--- PinkChallenge/AppDelegate.swift
+++ PinkChallenge/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
      
         window?.backgroundColor = UIColor.white
